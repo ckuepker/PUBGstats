81ff4f3afd3d1157e2cc7beadd29ea1381d2c173
Christoph Küpker <christoph.kuepker@ise.de>
2017-09-28 10:44:03 +0200

    Added mocked matches to MatchViewModel for UI creation

--- PUBGstats.ViewModel.Test/MatchListViewModelTest.cs
+++ PUBGstats.ViewModel.Test/MatchListViewModelTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace PUBGstats.ViewModel.Test
@@ -8,15 +9,22 @@ namespace PUBGstats.ViewModel.Test
     [Test]
     public void TestHasTestMatches()
     {
-      var sut = new MatchListViewModel();
-      Assert.AreEqual(3, sut.Matches.Count());
+      IMatchListViewModel sut = new MatchListViewModel();
+      Assert.AreEqual(3, sut.Matches.Count);
       for (int i = 0; i < 3; i++)
       {
-        Assert.AreEqual(i + 1, sut.Matches[i]);
+        Assert.AreEqual(i + 1, sut.Matches[i].Id);
         Assert.AreNotEqual(0, sut.Matches[i].Kills);
         Assert.AreNotEqual(0, sut.Matches[i].Rank);
         Assert.AreNotEqual(0, sut.Matches[i].Score);
       }
+    }
+
+    [Test]
+    public void TestHasNoTestMatchesAfterFeatureComplete()
+    {
+      var sut = new MatchListViewModel();
+      Assert.AreEqual(0, sut.Matches.Count);
     }
   }
 }
---
+++ PUBGstats.ViewModel/MatchListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PUBGstats.Match;
+using PUBGstats.Match.Builder;
+
+namespace PUBGstats.ViewModel
+{
+  public class MatchListViewModel : ViewModelBase, IMatchListViewModel
+  {
+    private IList<IMatch> _matches;
+
+    public IList<IMatch> Matches
+    {
+      get {
+        return _matches ?? (_matches = new List<IMatch> {
+          new MatchBuilder().WithId(1).WithKills(8).WithRank(2).WithScore(100).Build(),
+          new MatchBuilder().WithId(2).WithKills(1).WithRank(22).WithScore(70).Build(),
+          new MatchBuilder().WithId(3).WithKills(1).WithRank(65).WithScore(35).Build()
+        });
+      }
+      set { _matches = value; }
+    }
+  }
+}
